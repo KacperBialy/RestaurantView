eff49b90702de0f6ae1f47b13cfb239960dcffeb
kacper <kacper1400@o2.pl>
2021-07-02 15:50:25 +0200

    Adding retrieving data from login form

---
+++ SignIn.js
@@ -0,0 +1,11 @@
+function GetSingInValue()
+{
+const formData = document.querySelectorAll("#form input");
+const email = formData[0].value;
+const password = formData[1].value;
+Loggin(email,password);
+}
+function Loggin(email, pasword)
+{
+//TODO => Send POST request to api with login request
+}
